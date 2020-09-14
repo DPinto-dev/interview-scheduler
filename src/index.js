53abfb978d2886b4bd59afda52efee75459f4e44
DPintoLL <diogosmp@gmail.com>
2020-09-14 10:45:10 -0700

    Preparing Netlify config for deployment

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-if (process.env.REACT_APP_API_BASE_URL) {
-  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-}
-
 import "index.scss";
 
 import Application from "components/Application";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<Application />, document.getElementById("root"));
